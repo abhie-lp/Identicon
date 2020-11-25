002e240f8399e0475e988128a77df6584b6b12e8
Abhishek Tiwari <abhie.lp@gmail.com>
2020-11-25 20:16:58 +0530

    Convert the hash value to list

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,13 +5,25 @@ defmodule Identicon do
   def main(string) do
     string
     |> create_hash
+    |> hash_to_list
+  end
+
+  @doc """
+  Convert the hash value to list
+
+  ## Examples
+      iex> Identicon.hash_to_list(Identicon.create_hash("asdf"))
+      [145, 46, 200, 3, 178, 206, 73, 228, 165, 65, 6, 141, 73, 90, 181, 112]
+  """
+  def hash_to_list(hash_value) do
+    :binary.bin_to_list(hash_value)
   end
 
   @doc """
   Create the MD5 hash of the given string.
 
   ## Examples
-      iex(4)> Identicon.create_hash("asdf")
+      iex> Identicon.create_hash("asdf")
       <<145, 46, 200, 3, 178, 206, 73, 228, 165, 65, 6, 141, 73, 90, 181, 112>>
   """
   def create_hash(string) do
