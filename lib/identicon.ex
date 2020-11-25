e307ceae259851f8330ee0a6eb94f3bb8f06a22b
Abhishek Tiwari <abhie.lp@gmail.com>
2020-11-25 20:13:16 +0530

    Generate MD5 hash of given string

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,20 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for `Identicon`.
+  Generate and save the identicon image for the given string.
   """
+  def main(string) do
+    string
+    |> create_hash
+  end
 
   @doc """
-  Hello world.
+  Create the MD5 hash of the given string.
 
   ## Examples
-
-      iex> Identicon.hello()
-      :world
-
+      iex(4)> Identicon.create_hash("asdf")
+      <<145, 46, 200, 3, 178, 206, 73, 228, 165, 65, 6, 141, 73, 90, 181, 112>>
   """
-  def hello do
-    :world
+  def create_hash(string) do
+    :crypto.hash(:md5, string)
   end
 end
